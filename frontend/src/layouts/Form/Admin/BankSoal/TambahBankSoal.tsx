2e157bd400923f32a698ff114c649828b3e03e59
mustafamadjid <madjidmustafa@gmail.com>
2026-01-04 18:56:34 +0700

    FE : Fix show password login input field

---
+++ frontend/src/layouts/Form/Admin/BankSoal/TambahBankSoal.tsx
@@ -0,0 +1,8 @@
+
+export const TambahBankSoalForm = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
--- frontend/src/pages/Admin/Dashboard/BankSoal/TambahBankSoal.tsx
+++ frontend/src/pages/Admin/Dashboard/BankSoal/TambahBankSoal.tsx
@@ -1,6 +1,11 @@
+import { TambahBankSoalForm } from "@/layouts/Form/Admin/BankSoal/TambahBankSoal";
+
 export const TambahBankSoal = () => {
     return (
         <>
+        <div>
+            <TambahBankSoalForm />
+        </div>
         </>
     );
 }
