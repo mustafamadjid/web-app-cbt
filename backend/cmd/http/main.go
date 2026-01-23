3d9eabc8fa937259f4d4bae688aef919c9f45f15
mustafamadjid <madjidmustafa@gmail.com>
2026-01-23 10:55:47 +0700

    BE -> fix : delete env

--- backend/cmd/http/main.go
+++ backend/cmd/http/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/mustafamadjid/web-app-cbt/internal/infra/db"
+)
+
+func main() {
+	ctx := context.Background()
+	env, ok := os.LookupEnv("POSTGRES_DBURL")
+	if !ok {
+		log.Fatal("POSTGRES_DBURL tidak ada")
+	}
+	fmt.Println(env)
+
+	pool, err := db.OpenPgxPool(ctx,db.PgxConfig{
+		DbURL:             os.Getenv("POSTGRES_DBURL"),
+		MaxConns:        20,
+		MinConns:        5,
+		MaxConnLifetime: 30 * time.Minute,
+		MaxConnIdleTime: 5 * time.Minute,
+		HealthTimeout:   3 * time.Second,
+	})
+
+	if err != nil {
+		log.Fatalf("DB init failed: %v",err)
+		log.Fatal("Exiting....")
+	}
+	log.Println("DB init success")
+	defer pool.Close()
+}
