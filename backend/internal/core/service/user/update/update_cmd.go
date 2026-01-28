ebb2c0e86c3223c02e47edbca1c79b5b670d1dc1
Athaullah Mustafa Madjid_122140191 <76590503+mustafamadjid@users.noreply.github.com>
2026-01-28 22:29:14 +0700

    Add update siswa service and repo patch

--- backend/internal/core/service/user/update/update_cmd.go
+++ backend/internal/core/service/user/update/update_cmd.go
@@ -1,6 +1,10 @@
 package user_service
 
-import "github.com/mustafamadjid/web-app-cbt/internal/core/domain/user"
+import (
+	"time"
+
+	"github.com/mustafamadjid/web-app-cbt/internal/core/domain/user"
+)
 
 type UpdateGuruCmd struct {
 	IdPengguna user.ID
@@ -17,4 +21,25 @@ type UpdateGuruCmd struct {
 	Nip         *string
 	Jabatan     *string
 	BidangStudi *string
+}
+
+type UpdateSiswaCmd struct {
+	IdPengguna user.ID
+
+	Username     *string
+	Email        *string
+	NamaLengkap  *string
+	JenisKelamin *string
+	NoHp         *string
+	Foto         *string
+	StatusAkun   *user.StatusAkun
+	Role         *user.Role
+
+	IdTingkatKelas *user.ID
+	IdNamaKelas    *user.ID
+	Nisn           *string
+	NoAbsen        *int
+	Angkatan       *int
+	TempatLahir    *string
+	TanggalLahir   *time.Time
 }
