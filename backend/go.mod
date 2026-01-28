264df9e32462e71edc5f39807438deb17ac09e3b
mustafamadjid <madjidmustafa@gmail.com>
2026-01-28 10:12:12 +0700

    BE -> feat : tambah adapter untuk generate hash dan compare hash

--- backend/go.mod
+++ backend/go.mod
@@ -10,7 +10,8 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/text v0.29.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ backend/internal/adapter/securtity/bcrypt/hasher.go
@@ -0,0 +1,26 @@
+package bcrypt
+
+import "golang.org/x/crypto/bcrypt"
+
+type Hasher struct {
+	cost int
+}
+
+func NewHasher(cost int) *Hasher {
+	if cost == 0 {
+		cost = bcrypt.DefaultCost
+	}
+	return &Hasher{cost: cost}
+}
+
+func (h *Hasher) GenerateHash(plain string)(string,error){
+	b, error := bcrypt.GenerateFromPassword([]byte(plain),h.cost)
+	if error != nil {
+		return "", error
+	}
+	return string(b), nil
+}
+
+func (h *Hasher) CompareHash(hash, plain string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(plain)) == nil
+}
