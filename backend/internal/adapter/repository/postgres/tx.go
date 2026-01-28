0d62873a0886cec329c78a8b597759be3a70affc
Athaullah Mustafa Madjid_122140191 <76590503+mustafamadjid@users.noreply.github.com>
2026-01-28 09:39:24 +0700

    Implement postgres user and profile repositories

--- backend/internal/adapter/repository/postgres/tx.go
+++ backend/internal/adapter/repository/postgres/tx.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/jackc/pgx/v5"
 
-	outuser "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/user"
 	userrepo "github.com/mustafamadjid/web-app-cbt/internal/adapter/repository/postgres/user_repo"
+	outuser "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/user"
 )
 
 type pgTx struct {
@@ -16,4 +16,8 @@ type pgTx struct {
 
 func (p *pgTx) Pengguna() outuser.UserRepository {
 	return &userrepo.UserRepo{q: p.tx}
+}
+
+func (p *pgTx) ProfilGuru() outuser.ProfilGuruRepository {
+	return &userrepo.ProfilgGuruRepo{q: p.tx}
 }
