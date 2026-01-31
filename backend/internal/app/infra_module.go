b4d62c94618e5787cf76cdffab1404fff5315ee6
Athaullah Mustafa Madjid_122140191 <76590503+mustafamadjid@users.noreply.github.com>
2026-01-31 10:51:52 +0700

    Add guru list retrieval flow

--- backend/internal/app/infra_module.go
+++ backend/internal/app/infra_module.go
@@ -5,30 +5,32 @@ import (
 
 	pg "github.com/mustafamadjid/web-app-cbt/internal/adapter/repository/postgres"
 	out "github.com/mustafamadjid/web-app-cbt/internal/core/port/out"
-	outuser "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/user"
 	outauth "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/auth_port_out"
 	txout "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/tx"
+	outuser "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/user"
 )
 
 type InfraModule struct {
 	Pool *pgxpool.Pool
 	Txm  txout.TxManager
 
-	Sessions out.SessionRepository
-	AuthUsers outauth.AuthUserrepository
-	users 	outuser.UserRepository
+	Sessions    out.SessionRepository
+	AuthUsers   outauth.AuthUserrepository
+	users       outuser.UserRepository
 	profilSiswa outuser.GetListSiswaRepo
+	profilGuru  outuser.GetGuruListRepo
 }
 
 func BuildInfraModule(pool *pgxpool.Pool) *InfraModule {
 	txm := pg.NewTxManager(pool)
 
 	return &InfraModule{
-		Pool:     pool,
-		Txm:      txm,
-		Sessions: pg.NewSessionRepo(pool),
-		AuthUsers: pg.NewAuthUserRepo(pool),
-		users : pg.NewUserRepo(pool),
+		Pool:        pool,
+		Txm:         txm,
+		Sessions:    pg.NewSessionRepo(pool),
+		AuthUsers:   pg.NewAuthUserRepo(pool),
+		users:       pg.NewUserRepo(pool),
 		profilSiswa: pg.NewProfilSiswaRepo(pool),
+		profilGuru:  pg.NewProfilgGuruRepo(pool),
 	}
 }
