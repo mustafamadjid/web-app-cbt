94b92322e8164cb2fc6921c6ebbb92c4e7cd5579
Athaullah Mustafa Madjid_122140191 <76590503+mustafamadjid@users.noreply.github.com>
2026-02-10 19:23:38 +0700

    test: add branch-coverage tests for kelas create service

---
+++ backend/internal/core/service/kelas/create/fake_test/repo.go
@@ -0,0 +1,56 @@
+package fake_test
+
+import (
+	"context"
+
+	"github.com/mustafamadjid/web-app-cbt/internal/core/domain/kelas"
+	query "github.com/mustafamadjid/web-app-cbt/internal/core/query/kelas"
+)
+
+type FakeKelasRepo struct {
+	ExistTingkatKelasRet  bool
+	ExistTingkatKelasErr  error
+	CreateTingkatKelasErr error
+
+	ExistNamaKelasRet  bool
+	ExistNamaKelasErr  error
+	CreateNamaKelasErr error
+
+	ExistTingkatKelasCalled  bool
+	CreateTingkatKelasCalled bool
+	ExistNamaKelasCalled     bool
+	CreateNamaKelasCalled    bool
+
+	GotExistTingkatKelas  int
+	GotCreateTingkatKelas int
+	GotExistNamaKelas     string
+	GotCreateNamaKelas    kelas.NamaKelas
+}
+
+func (f *FakeKelasRepo) GetKelas(_ context.Context, _ query.ListKelasFilter) ([]kelas.FullKelasData, error) {
+	return nil, nil
+}
+
+func (f *FakeKelasRepo) CreateTingkatKelas(_ context.Context, tingkatKelas int) error {
+	f.CreateTingkatKelasCalled = true
+	f.GotCreateTingkatKelas = tingkatKelas
+	return f.CreateTingkatKelasErr
+}
+
+func (f *FakeKelasRepo) CreateNamaKelas(_ context.Context, namaKelas kelas.NamaKelas) error {
+	f.CreateNamaKelasCalled = true
+	f.GotCreateNamaKelas = namaKelas
+	return f.CreateNamaKelasErr
+}
+
+func (f *FakeKelasRepo) ExistTingkatKelas(_ context.Context, tingkatKelas int) (bool, error) {
+	f.ExistTingkatKelasCalled = true
+	f.GotExistTingkatKelas = tingkatKelas
+	return f.ExistTingkatKelasRet, f.ExistTingkatKelasErr
+}
+
+func (f *FakeKelasRepo) ExistNamaKelas(_ context.Context, namaKelas string) (bool, error) {
+	f.ExistNamaKelasCalled = true
+	f.GotExistNamaKelas = namaKelas
+	return f.ExistNamaKelasRet, f.ExistNamaKelasErr
+}
