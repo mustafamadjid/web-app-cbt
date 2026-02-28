469da417ad332e9b193974b3998e4b1d04323026
mustafamadjid <madjidmustafa@gmail.com>
2026-02-28 12:01:00 +0700

    BE -> feat : update service

--- backend/internal/core/service/bank_soal/update/update_service.go
+++ backend/internal/core/service/bank_soal/update/update_service.go
@@ -3,8 +3,10 @@ package bank_soal_service
 import (
 	"context"
 
+	coreerror "github.com/mustafamadjid/web-app-cbt/internal/core/core_error"
 	"github.com/mustafamadjid/web-app-cbt/internal/core/domain/bank_soal"
 	bank_soal_repo "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/bank_soal"
+	corelog "github.com/mustafamadjid/web-app-cbt/internal/core/port/out/log"
 )
 
 type UpdateBankSoalService struct {
@@ -18,5 +20,14 @@ func NewUpdateBankSoalService(repo bank_soal_repo.BankSoalRepository) *UpdateBan
 }
 
 func(r *UpdateBankSoalService) UpdateBankSoalService(ctx context.Context, idBankSoal bank_soal.ID, payload bank_soal.BankSoal) (bank_soal.BankSoal, error){
-	
+	logger := corelog.FromContext(ctx)
+
+	if idBankSoal <= 0 {
+		logger.Error(ctx, "failed update bank soal", "layer", "core.service", "op", "bank_soal.update", "err", coreerror.ErrMissingId)
+		return bank_soal.BankSoal{}, nil
+	}
+
+	if payload.NamaBankSoal != nil {
+		
+	}
 }
